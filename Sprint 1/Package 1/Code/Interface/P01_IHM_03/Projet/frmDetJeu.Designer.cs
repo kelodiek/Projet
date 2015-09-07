396184b1a2e32edcc0bceeaebb7dd324db401cc4
Olivier Pelletier <shooter994@gmail.com>
2015-09-07 15:35:49 -0400

    intereface

---
+++ Sprint 1/Package 1/Code/Interface/P01_IHM_03/Projet/frmDetJeu.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace Projet
+{
+    partial class frmDetJeu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAjoutMode = new System.Windows.Forms.Button();
+            this.lstMode = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAjoutTheme = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.RichTextBox();
+            this.Label10 = new System.Windows.Forms.Label();
+            this.btnAjoutGenre = new System.Windows.Forms.Button();
+            this.TextBox7 = new System.Windows.Forms.TextBox();
+            this.Label9 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.lstTheme = new System.Windows.Forms.ComboBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.lstGenre = new System.Windows.Forms.ComboBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtEtat = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAjoutMode
+            // 
+            this.btnAjoutMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjoutMode.Location = new System.Drawing.Point(357, 372);
+            this.btnAjoutMode.Name = "btnAjoutMode";
+            this.btnAjoutMode.Size = new System.Drawing.Size(27, 24);
+            this.btnAjoutMode.TabIndex = 99;
+            this.btnAjoutMode.Text = "+";
+            this.btnAjoutMode.UseVisualStyleBackColor = true;
+            // 
+            // lstMode
+            // 
+            this.lstMode.FormattingEnabled = true;
+            this.lstMode.Location = new System.Drawing.Point(147, 372);
+            this.lstMode.Name = "lstMode";
+            this.lstMode.Size = new System.Drawing.Size(200, 24);
+            this.lstMode.TabIndex = 98;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(10, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 97;
+            this.label2.Text = "Mode : ";
+            // 
+            // btnAjoutTheme
+            // 
+            this.btnAjoutTheme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjoutTheme.Location = new System.Drawing.Point(357, 342);
+            this.btnAjoutTheme.Name = "btnAjoutTheme";
+            this.btnAjoutTheme.Size = new System.Drawing.Size(27, 24);
+            this.btnAjoutTheme.TabIndex = 96;
+            this.btnAjoutTheme.Text = "+";
+            this.btnAjoutTheme.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(9, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 95;
+            this.label1.Text = "Date de Sortie : ";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(147, 279);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 94;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(10, 469);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(425, 183);
+            this.richTextBox2.TabIndex = 93;
+            this.richTextBox2.Text = "";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(10, 446);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(246, 20);
+            this.label12.TabIndex = 92;
+            this.label12.Text = "Informations Supplémentaires : ";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(10, 85);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(425, 160);
+            this.txtDesc.TabIndex = 91;
+            this.txtDesc.Text = "";
+            // 
+            // Label10
+            // 
+            this.Label10.AutoSize = true;
+            this.Label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label10.Location = new System.Drawing.Point(9, 62);
+            this.Label10.Name = "Label10";
+            this.Label10.Size = new System.Drawing.Size(165, 20);
+            this.Label10.TabIndex = 90;
+            this.Label10.Text = "Description du Jeu : ";
+            // 
+            // btnAjoutGenre
+            // 
+            this.btnAjoutGenre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjoutGenre.Location = new System.Drawing.Point(357, 309);
+            this.btnAjoutGenre.Name = "btnAjoutGenre";
+            this.btnAjoutGenre.Size = new System.Drawing.Size(27, 24);
+            this.btnAjoutGenre.TabIndex = 89;
+            this.btnAjoutGenre.Text = "+";
+            this.btnAjoutGenre.UseVisualStyleBackColor = true;
+            this.btnAjoutGenre.Click += new System.EventHandler(this.btnAjoutGenre_Click);
+            // 
+            // TextBox7
+            // 
+            this.TextBox7.Location = new System.Drawing.Point(147, 408);
+            this.TextBox7.Name = "TextBox7";
+            this.TextBox7.Size = new System.Drawing.Size(200, 22);
+            this.TextBox7.TabIndex = 88;
+            // 
+            // Label9
+            // 
+            this.Label9.AutoSize = true;
+            this.Label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label9.Location = new System.Drawing.Point(10, 410);
+            this.Label9.Name = "Label9";
+            this.Label9.Size = new System.Drawing.Size(81, 20);
+            this.Label9.TabIndex = 87;
+            this.Label9.Text = "Version : ";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label5.Location = new System.Drawing.Point(10, 344);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(75, 20);
+            this.Label5.TabIndex = 86;
+            this.Label5.Text = "Thème : ";
+            // 
+            // lstTheme
+            // 
+            this.lstTheme.FormattingEnabled = true;
+            this.lstTheme.Location = new System.Drawing.Point(147, 342);
+            this.lstTheme.Name = "lstTheme";
+            this.lstTheme.Size = new System.Drawing.Size(200, 24);
+            this.lstTheme.TabIndex = 85;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label4.Location = new System.Drawing.Point(10, 311);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(70, 20);
+            this.Label4.TabIndex = 84;
+            this.Label4.Text = "Genre : ";
+            // 
+            // lstGenre
+            // 
+            this.lstGenre.FormattingEnabled = true;
+            this.lstGenre.Location = new System.Drawing.Point(147, 307);
+            this.lstGenre.Name = "lstGenre";
+            this.lstGenre.Size = new System.Drawing.Size(200, 24);
+            this.lstGenre.TabIndex = 83;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(147, 33);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(159, 22);
+            this.txtNom.TabIndex = 82;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(10, 37);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(54, 20);
+            this.Label3.TabIndex = 81;
+            this.Label3.Text = "Nom :";
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(147, 5);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(50, 22);
+            this.txtID.TabIndex = 80;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(10, 7);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(91, 20);
+            this.lblID.TabIndex = 79;
+            this.lblID.Text = "Identifiant :";
+            // 
+            // txtEtat
+            // 
+            this.txtEtat.Location = new System.Drawing.Point(147, 251);
+            this.txtEtat.Name = "txtEtat";
+            this.txtEtat.Size = new System.Drawing.Size(200, 22);
+            this.txtEtat.TabIndex = 78;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(10, 253);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(49, 20);
+            this.label11.TabIndex = 77;
+            this.label11.Text = "État :";
+            // 
+            // frmDetJeu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 705);
+            this.Controls.Add(this.btnAjoutMode);
+            this.Controls.Add(this.lstMode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAjoutTheme);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.Label10);
+            this.Controls.Add(this.btnAjoutGenre);
+            this.Controls.Add(this.TextBox7);
+            this.Controls.Add(this.Label9);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.lstTheme);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.lstGenre);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtEtat);
+            this.Controls.Add(this.label11);
+            this.Name = "frmDetJeu";
+            this.Text = "Detail - Jeu";
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.txtEtat, 0);
+            this.Controls.SetChildIndex(this.lblID, 0);
+            this.Controls.SetChildIndex(this.txtID, 0);
+            this.Controls.SetChildIndex(this.Label3, 0);
+            this.Controls.SetChildIndex(this.txtNom, 0);
+            this.Controls.SetChildIndex(this.lstGenre, 0);
+            this.Controls.SetChildIndex(this.Label4, 0);
+            this.Controls.SetChildIndex(this.lstTheme, 0);
+            this.Controls.SetChildIndex(this.Label5, 0);
+            this.Controls.SetChildIndex(this.Label9, 0);
+            this.Controls.SetChildIndex(this.TextBox7, 0);
+            this.Controls.SetChildIndex(this.btnAjoutGenre, 0);
+            this.Controls.SetChildIndex(this.Label10, 0);
+            this.Controls.SetChildIndex(this.txtDesc, 0);
+            this.Controls.SetChildIndex(this.label12, 0);
+            this.Controls.SetChildIndex(this.richTextBox2, 0);
+            this.Controls.SetChildIndex(this.dateTimePicker1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.btnAjoutTheme, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.lstMode, 0);
+            this.Controls.SetChildIndex(this.btnAjoutMode, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnAjoutMode;
+        internal System.Windows.Forms.ComboBox lstMode;
+        private System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Button btnAjoutTheme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        internal System.Windows.Forms.RichTextBox richTextBox2;
+        internal System.Windows.Forms.Label label12;
+        internal System.Windows.Forms.RichTextBox txtDesc;
+        internal System.Windows.Forms.Label Label10;
+        internal System.Windows.Forms.Button btnAjoutGenre;
+        internal System.Windows.Forms.TextBox TextBox7;
+        internal System.Windows.Forms.Label Label9;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.ComboBox lstTheme;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.ComboBox lstGenre;
+        internal System.Windows.Forms.TextBox txtNom;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtID;
+        internal System.Windows.Forms.Label lblID;
+        internal System.Windows.Forms.TextBox txtEtat;
+        internal System.Windows.Forms.Label label11;
+    }
+}
