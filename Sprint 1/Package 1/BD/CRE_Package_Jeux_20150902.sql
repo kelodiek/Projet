8c70490ddf2c9af28ecef80c5c16784079f6587a
Élodie <kerel1330243@etu.cegepjonquiere.ca>
2015-09-09 14:33:04 -0400

    maj DCR

--- Sprint 1/Package 1/BD/CRE_Package_Jeux_20150902.sql
+++ Sprint 1/Package 1/BD/CRE_Package_Jeux_20150902.sql
@@ -1,11 +1,19 @@
-use dbProjetE2;
-/* pas sur du use au cas ou la bd changerais de nom  ???  A VÉRIFIER*/
+use dbProjetE2Test;
 
-GO
-IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'Jeux')
+IF EXISTS (SELECT * FROM sys.schemas WHERE name = 'Jeux')
+BEGIN
+	EXEC( 'DROP TABLE Jeux.tblSysExp' );
+	EXEC( 'DROP TABLE Jeux.tblCategorie' );
+
+	
+	EXEC( 'DROP SCHEMA Jeux' );
+END
+ELSE
 BEGIN
     EXEC( 'CREATE SCHEMA Jeux' );
 END
+
+
 
 GO
 CREATE TABLE Jeux.tblSysExp
@@ -19,6 +27,15 @@ InfoSupSysExp	TEXT		NULL
 )
 GO
 PRINT 'Création de Jeux.tblSysExp complétée'
+GO
+CREATE TABLE Jeux.tblCategorie
+(
+CodeCategorie	VARCHAR(7)		NOT NULL,
+DescCategorie	VARCHAR(40)		NOT NULL,
+ComCategorie	VARCHAR(150)	NULL
+)
+GO
+PRINT 'Création de Jeux.tblCategorie complétée'
 GO
 
 use master;
